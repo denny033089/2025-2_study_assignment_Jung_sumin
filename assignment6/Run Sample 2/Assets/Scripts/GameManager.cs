cef2f57e3de6f7969c7c04a9dfb0a9d1876e365c
denny033089 <denny033089@gmail.com>
2025-11-24 22:18:05 +0900

    change 11.24
    정수민 과제6

--- assignment6/Run Sample 2/Assets/Scripts/GameManager.cs
+++ assignment6/Run Sample 2/Assets/Scripts/GameManager.cs
@@ -36,7 +36,10 @@ public class GameManager : MonoBehaviour
     {
         // CamObj는 Character의 x, y position을 따라간다.
         // ---------- TODO ---------- 
-        
+        Vector3 Campos = new Vector3(Character.transform.position.x,
+                                    Character.transform.position.y,
+                                    CamObj.transform.position.z);
+        CamObj.transform.position = Campos;
         // -------------------- 
     }
 
@@ -44,7 +47,7 @@ public class GameManager : MonoBehaviour
     {
         // Character는 초당 CharacterSpeed의 속도로 우측으로 움직인다.
         // ---------- TODO ---------- 
-        
+        Character.transform.position += new Vector3(1,0,0)*CharacterSpeed*Time.deltaTime;
         // -------------------- 
     }
 
@@ -52,6 +55,9 @@ public class GameManager : MonoBehaviour
     {
         // Character를 삭제하고, "Game Over!"라는 메시지를 3초간 띄우고, RestartButton을 활성화한다.
         // ---------- TODO ---------- 
+        Destroy(Character);
+        MyUIManager.DisplayMessage("GameOver",3f);
+        MyUIManager.RestartButton.SetActive(true);
         
         // -------------------- 
     }
@@ -60,7 +66,8 @@ public class GameManager : MonoBehaviour
     {
         // point만큼 점수를 증가시키고 UI에 표시한다.
         // ---------- TODO ---------- 
-        
+        NowScore +=point;
+        MyUIManager.DisplayScore(NowScore);
         // -------------------- 
     }
 
